723451f48df9932dd28e4254ec2506407ce9eb1e
NataliiaPasaman <natapasaman@gmail.com>
2022-09-02 21:07:05 +0300

    task-3 -finally

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,5 @@
-import { throttle } from "lodash.throttle";
-/**HTML містить розмітку форми. Напиши скрипт, який буде зберігати значення полів у локальне сховище, 
- * коли користувач щось друкує.
+import throttle from "lodash.throttle";
 
-Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, 
-у яких зберігай поточні значення полів форми. 
-Нехай ключем для сховища буде рядок "feedback-form-state".
-
-
-Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. 
-В іншому випадку поля повинні бути порожніми.
-
-Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, 
-message та їхніми поточними значеннями.
-
-Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.  */
 const STORAGE_KEY = "feedback-form-state";
 
 const form = document.querySelector('.feedback-form');
@@ -21,23 +7,24 @@ const inputEmail = document.querySelector('input');
 const textareaMessage = document.querySelector('textarea');
 
 form.addEventListener('submit', onFormSubmit);
-form.addEventListener('input', onFormInput);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
 getData();
 
-function onFormInput(evt) {
-    const storageData = {
-        mail: evt.target.value,
-        message: evt.currentTarget.value,
+function onFormInput() {
+    storageData = {
+        mail: inputEmail.value,
+        message: textareaMessage.value,
     };
-  
+
     localStorage.setItem(STORAGE_KEY, JSON.stringify(storageData));
 }
 
 function getData() {
-    const savedData = localStorage.getItem(STORAGE_KEY);
-    if (savedData) {
-        // inputEmail.value = JSON.parse(savedData);
-        inputEmail.value = savedData;
+    if (parsedData) {
+        inputEmail.value = parsedData.mail;
+        textareaMessage.value = parsedData.message;
     }
 }
 
@@ -45,4 +32,7 @@ function onFormSubmit(evt) {
     evt.preventDefault();
     form.reset();
     localStorage.removeItem(STORAGE_KEY);
+
+    console.log(parsedData);
 }
+
