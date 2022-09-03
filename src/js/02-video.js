2cb5c4405908b4e4b9890241b57d46fac11fedf4
NataliiaPasaman <natapasaman@gmail.com>
2022-09-03 11:00:09 +0300

    task-2 fixed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,50 +1,30 @@
 import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
-/** 
 
-Під час перезавантаження сторінки скористайся методом setCurrentTime() 
-з метою відновлення відтворення зі збереженої позиції.
-Додай до проекту бібілотеку lodash.throttle і зроби так, щоб час в
-ідтворення оновлювався у сховищі не частіше, ніж раз на секунду.
- */
 const PLAY_TIME = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', onStopTime);
+player.on('timeupdate', throttle(onStopTime, 1000));
 
 function onStopTime(timeUpdate) {
-  let time = timeUpdate.seconds;
-  console.log(time);
+  let timePause = timeUpdate.seconds;
+  console.log(timePause);
 
-  localStorage.setItem(PLAY_TIME, time);
+  localStorage.setItem(PLAY_TIME, timePause);
 }
 
+const pause = localStorage.getItem(PLAY_TIME);
 
-
-
-const a = localStorage.getItem(PLAY_TIME);
-const b = Number(a);
-console.log(b);
-
-const c = player.getCurrentTime().then(function(b) {
-  // seconds = the current playback position
-  console.log(b);
+player.setCurrentTime(pause).then(function() {
+  console.log(pause);
 }).catch(function(error) {
-  // an error occurred
+  switch (error.name) {
+      case 'RangeError':
+          break;
+
+      default:
+          break;
+  }
 });
-
-// player.setCurrentTime(b).then(function(seconds) {
-//   // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//   switch (error.name) {
-//       case 'RangeError':
-//           // the time was less than 0 or greater than the video’s duration
-//           break;
-
-//       default:
-//           // some other error occurred
-//           break;
-//   }
-// });
